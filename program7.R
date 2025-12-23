9bf52f8be1a903fa3207fd860fe71e36c0da291c
Vamsiyadav1472 <jaganvamsi92@gmail.com>
2025-12-23 12:51:36 +0530

    program7.R

--- program7.R
+++ program7.R
@@ -1,10 +1,18 @@
-# Weight data
-weight <- c(60, 65, 70, 75, 80, 85, 90, 95, 100)
+library(ggplot2)
 
-# Create density plot
-plot(density(weight),
-     main = "Density Plot of Weights",
-     xlab = "Weight (kg)",
-     ylab = "Density",
-     col = "purple",
-     lwd = 2)
+# Create a data frame
+scores_group_data <- data.frame(
+  Group = c("X", "X", "X", "X", "Y", "Y", "Y", "Y"),
+  Score = c(80, 85, 78, 92, 88, 76, 80, 84)
+)
+
+# Violin plot
+ggplot(scores_group_data, aes(x = Group, y = Score, fill = Group)) +
+  geom_violin(trim = FALSE) +
+  labs(
+    title = "Scores by Group",
+    x = "Group",
+    y = "Score"
+  ) +
+  theme_minimal()
+
