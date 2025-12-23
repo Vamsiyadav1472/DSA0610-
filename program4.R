6419f86f47ef5218043ce0e88d57a71efd907a68
Vamsiyadav1472 <jaganvamsi92@gmail.com>
2025-12-23 12:49:52 +0530

    program4.R

--- program4.R
+++ program4.R
@@ -1,11 +1,22 @@
-# Age data
-age <- c(25, 30, 35, 40, 45, 50, 55, 60, 65, 70)
+library(ggplot2)
 
-# Create histogram
-hist(age,
-     breaks = 5,              # number of bins
-     col = "orange",
-     main = "Age Distribution Histogram",
-     xlab = "Age",
-     ylab = "Frequency",
-     border = "brown")
+# Create a data frame
+age_data <- data.frame(
+  Age = c(25, 30, 35, 40, 45, 50, 55, 60, 65, 70)
+)
+
+# Histogram
+ggplot(age_data, aes(x = Age)) +
+  geom_histogram(
+    binwidth = 5,
+    fill = "purple",
+    color = "black",
+    alpha = 0.7
+  ) +
+  labs(
+    title = "Age Distribution",
+    x = "Age",
+    y = "Frequency"
+  ) +
+  theme_minimal()
+
